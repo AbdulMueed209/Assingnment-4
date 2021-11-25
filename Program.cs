6188f99294d6dab54e23c671c54eda66e18a8cc9
ABDUL MUEED <abdulmueed209@gmail.com>
2021-11-26 01:37:22 +0500

    FINISHED

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assingnment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
